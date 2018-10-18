e89bd4671727f28a4e3df4ab47deafec3ca4b2f9
myt <393311231@qq.com>
2018-10-18 21:12:57 +0800

    加入显示天气信息的功能

--- app/src/main/java/app/coolweather/com/util/Utility.java
+++ app/src/main/java/app/coolweather/com/util/Utility.java
@@ -1,11 +1,18 @@
 package app.coolweather.com.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.Locale;
+import java.util.prefs.PreferenceChangeEvent;
 
 import app.coolweather.com.db.CoolWeatherDB;
 import app.coolweather.com.model.City;
@@ -78,5 +85,35 @@ public class Utility {
             }
         }
         return true;
+    }
+
+    public static void handleWeatherResponse(Context context, String response){
+        try{
+            JSONObject jsonObject=new JSONObject(response);
+            JSONObject weatherinfo=jsonObject.getJSONObject("weatherinfo");
+            String cityName=weatherinfo.getString("city");
+            String weatherCode=weatherinfo.getString("cityid");
+            String temp1=weatherinfo.getString("temp1");
+            String temp2=weatherinfo.getString("temp2");
+            String weatherDesp=weatherinfo.getString("weather");
+            String publishTime=weatherinfo.getString("ptime");
+            saveWeatherInfo(context,cityName,weatherCode,temp1,temp2,weatherDesp,publishTime);
+        }catch (JSONException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void saveWeatherInfo(Context context,String cityName,String weatherCode,String temp1,String temp2,String weatherDesp,String publishTime){
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name",cityName);
+        editor.putString("weather_code",weatherCode);
+        editor.putString("temp1",temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp",weatherDesp);
+        editor.putString("publish_time",publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
     }
 }
